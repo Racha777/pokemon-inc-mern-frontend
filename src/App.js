4b61c733f810fdd6c1fd1585dc387c46e06d4204
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-19 17:07:18 -0500

    Configuración de react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import {
+  Routes,
+  Route
+} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <>Pokémon Inc MERN</>
+    <Routes>
+      <Route path='/' element={<h2>Home</h2>} />
+      <Route path='/create-pokemon' element={<h2>create-pokemon</h2>} />
+      <Route path='/update-pokemon' element={<h2>update-pokemon</h2>} />
+      <Route path='*' element={<h2>404</h2>} />
+    </Routes>
   );
 }
 
