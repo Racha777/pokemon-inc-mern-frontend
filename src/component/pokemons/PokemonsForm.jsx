3e7ceda9817965d81d12e1d7d0cc0e411d4ccc75
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-20 17:42:43 -0500

    Update component PokemonsForm

--- src/component/pokemons/PokemonsForm.jsx
+++ src/component/pokemons/PokemonsForm.jsx
@@ -1,6 +1,35 @@
+import { Field, Form, Formik } from "formik";
+
 const PokemonsForm = () => {
   return (
-    <div>PokemonsForm</div>
+    <Formik
+      initialValues={{
+        name: '',
+        type: '',
+        hp: '',
+        attack: '',
+        special: ''
+      }}
+      onSubmit={(values, actions) => {
+        console.log(values);
+      }}
+    >
+      {({ handleSubmit }) => {
+        return (
+          <Form
+            onSubmit={handleSubmit}
+            className="text-black"
+          >
+            <Field name='name' placeholder="Nombre" />
+            <Field name='type' placeholder="Tipo" />
+            <Field name='hp' placeholder="Hp" />
+            <Field name='attack' placeholder="Ataque" />
+            <Field name='special' placeholder="Especial" />
+            <button type="submit">Crear</button>
+          </Form>
+        );
+      }}
+    </Formik>
   );
 }
 
