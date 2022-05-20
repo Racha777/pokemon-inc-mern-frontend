62c703f81dd234eb2a0c6809d0301b3e7dd6cbe0
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-20 18:19:17 -0500

    Update component PokemonsForm

--- src/component/pokemons/PokemonsForm.jsx
+++ src/component/pokemons/PokemonsForm.jsx
@@ -1,4 +1,5 @@
-import { Field, Form, Formik } from "formik";
+import * as yup from 'yup';
+import { ErrorMessage, Field, Form, Formik } from "formik";
 
 const PokemonsForm = () => {
   return (
@@ -8,8 +9,17 @@ const PokemonsForm = () => {
         type: '',
         hp: '',
         attack: '',
-        special: ''
+        special: '',
+        image: ''
       }}
+      validationSchema={yup.object({
+        name: yup.string().required('El nombre es requerido'),
+        type: yup.string().required('El tipo es requerido'),
+        hp: yup.number().required('El Hp es requerido'),
+        attack: yup.string().required('El ataque es requerido'),
+        special: yup.string().required('El especial es requerido'),
+        image: yup.string().required('La imagen es requerida')
+      })}
       onSubmit={(values, actions) => {
         console.log(values);
       }}
@@ -20,11 +30,18 @@ const PokemonsForm = () => {
             onSubmit={handleSubmit}
             className="text-black"
           >
-            <Field name='name' placeholder="Nombre" />
-            <Field name='type' placeholder="Tipo" />
-            <Field name='hp' placeholder="Hp" />
-            <Field name='attack' placeholder="Ataque" />
-            <Field name='special' placeholder="Especial" />
+            <Field name="name" placeholder="Nombre" />
+            <ErrorMessage name="name" />
+            <Field name="type" placeholder="Tipo" />
+            <ErrorMessage name="type" />
+            <Field name="hp" placeholder="Hp" />
+            <ErrorMessage name="hp" />
+            <Field name="attack" placeholder="Ataque" />
+            <ErrorMessage name="attack" />
+            <Field name="special" placeholder="Especial" />
+            <ErrorMessage name="special" />
+            <Field name="image" placeholder="Imagen" />
+            <ErrorMessage name="image" />
             <button type="submit">Crear</button>
           </Form>
         );
