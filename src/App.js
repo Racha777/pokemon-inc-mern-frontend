4973f1a3f9e9a2e1f8f5478697449e0c88c8a79c
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-20 13:30:11 -0500

    Update App.js PokemonProvider

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import {
   Route
 } from 'react-router-dom';
 import './App.css';
+import { PokemonProvider } from './context/PokemonContext';
 import PokemonLayout from './layouts/PokemonLayout';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<PokemonLayout />}>
-        <Route path='/create-pokemon' element={<h2>create-pokemon</h2>} />
-        <Route path='/update-pokemon' element={<h2>update-pokemon</h2>} />
-        <Route index element={<h2>Home</h2>} />
-      </Route>
-      <Route path='*' element={<h2>404</h2>} />
-    </Routes>
+    <PokemonProvider>
+      <Routes>
+        <Route path='/' element={<PokemonLayout />}>
+          <Route path='/create-pokemon' element={<h2>create-pokemon</h2>} />
+          <Route path='/update-pokemon' element={<h2>update-pokemon</h2>} />
+          <Route index element={<h2>Home</h2>} />
+        </Route>
+        <Route path='*' element={<h2>404</h2>} />
+      </Routes>
+    </PokemonProvider>
   );
 }
 
