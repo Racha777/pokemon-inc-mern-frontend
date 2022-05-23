0b755d11af6f8313661f8afd9fdb2060424eaa67
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-23 00:02:52 -0500

    component PokemonsCard

---
+++ src/component/pokemons/PokemonsCard.jsx
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom";
+
+const PokemonsCard = ({ pokemon }) => {
+  const { _id, name, type, hp, attack, special, image } = pokemon;
+  return (
+    <div
+      className="flex flex-col items-center gap-4 bg-white border border-cyan-300 rounded-lg shadow-sm shadow-zinc-100 max-w-xs p-4 dark:bg-zinc-700 dark:border-cyan-300"
+    >
+      <div className="w-32 h-32">
+        <img className="w-full h-full object-cover" src={image.url} alt={name} />
+      </div>
+      <div className="flex flex-col gap-1">
+        <p className="text-zinc-600 text-sm font-thin dark:text-zinc-400">Hp: {hp}</p>
+        <h5 className="text-black text-xl font-medium dark:text-white">{name}</h5>
+        <p className="text-zinc-600 text-sm font-extralight dark:text-zinc-400">{type}</p>
+      </div>
+      <div className="flex gap-1">
+        <span className="bg-zinc-800 rounded-xl text-white text-sm font-normal px-3 py-1">{attack}</span>
+        <span className="bg-zinc-800 rounded-xl text-white text-sm font-normal px-3 py-1">{special}</span>
+      </div>
+      <div className="flex gap-2">
+        <Link to="/" className="bg-cyan-300 rounded-md text-black font-medium px-3 py-1 cursor-pointer transition-colors hover:bg-white">
+          Editar
+        </Link>
+        <Link to="/" className="bg-rose-500 rounded-md text-white font-medium px-3 py-1 cursor-pointer transition-colors hover:bg-rose-600">
+          Eliminar
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default PokemonsCard;
--- src/pages/pokemons/PokemonsRead.jsx
+++ src/pages/pokemons/PokemonsRead.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import PokemonsCard from "../../component/pokemons/PokemonsCard";
 import usePokemon from "../../hooks/usePokemon";
 
 const PokemonsRead = () => {
@@ -15,33 +16,11 @@ const PokemonsRead = () => {
         <div className="grid justify-center md:grid-cols-4 gap-8">
           {
             pokemons.map((pokemon) => {
-              const { _id, name, type, hp, attack, special, image } = pokemon;
               return (
-                <div
-                  key={_id}
-                  className="flex flex-col items-center gap-4 bg-white border border-cyan-300 rounded-lg shadow-sm shadow-zinc-100 max-w-xs p-4 dark:bg-zinc-700 dark:border-cyan-300"
-                >
-                  <div className="w-32 h-32">
-                    <img className="w-full h-full object-cover" src={image.url} alt={name} />
-                  </div>
-                  <div className="flex flex-col gap-1">
-                    <p className="text-zinc-600 text-sm font-thin dark:text-zinc-400">Hp: {hp}</p>
-                    <h5 className="text-black text-xl font-medium dark:text-white">{name}</h5>
-                    <p className="text-zinc-600 text-sm font-extralight dark:text-zinc-400">{type}</p>
-                  </div>
-                  <div className="flex gap-1">
-                    <span className="bg-zinc-800 rounded-xl text-white text-sm font-normal px-3 py-1">{attack}</span>
-                    <span className="bg-zinc-800 rounded-xl text-white text-sm font-normal px-3 py-1">{special}</span>
-                  </div>
-                  <div className="flex gap-2">
-                    <Link to="/" className="bg-cyan-300 rounded-md text-black font-medium px-3 py-1 cursor-pointer transition-colors hover:bg-white">
-                      Editar
-                    </Link>
-                    <Link to="/" className="bg-rose-500 rounded-md text-white font-medium px-3 py-1 cursor-pointer transition-colors hover:bg-rose-600">
-                      Eliminar
-                    </Link>
-                  </div>
-                </div>
+                <PokemonsCard
+                  key={pokemon._id}
+                  pokemon={pokemon}
+                />
               );
             })
           }
