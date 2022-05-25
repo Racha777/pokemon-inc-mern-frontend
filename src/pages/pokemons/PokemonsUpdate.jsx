90db779db6d5b5328420d52f4436cdb8f7986940
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-25 18:58:29 -0500

    Update page PokemonsUpdate

--- src/pages/pokemons/PokemonsUpdate.jsx
+++ src/pages/pokemons/PokemonsUpdate.jsx
@@ -12,9 +12,11 @@ const PokemonsUpdate = () => {
   }, []);
 
   return (
-    <section>
-      <h2>PokemonsUpdate</h2>
-      <PokemonsForm />
+    <section className="py-8">
+      <div className="flex flex-col gap-8 container mx-auto px-8 md:px-4">
+        <h2 className="text-3xl text-yellow-400 font-bold">Actualizar Pokémon</h2>
+        <PokemonsForm />
+      </div>
     </section>
   );
 }
