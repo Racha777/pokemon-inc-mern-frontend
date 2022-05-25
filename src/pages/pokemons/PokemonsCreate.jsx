192cd468fa68ba5b19423c97076d32b954857cae
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-25 18:53:23 -0500

    Update page PokemonsCreate

--- src/pages/pokemons/PokemonsCreate.jsx
+++ src/pages/pokemons/PokemonsCreate.jsx
@@ -2,9 +2,11 @@ import PokemonsForm from "../../component/pokemons/PokemonsForm";
 
 const PokemonsCreate = () => {
   return (
-    <section>
-      <h2>PokemonsCreate</h2>
-      <PokemonsForm />
+    <section className="py-8">
+      <div className="flex flex-col gap-8 container mx-auto px-8 md:px-4">
+        <h2 className="text-3xl text-yellow-400 font-bold">Crear Pokémon</h2>
+        <PokemonsForm />
+      </div>
     </section>
   );
 }
