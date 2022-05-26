e7be1f9bcefc013a6ac15154453e449cd0baab92
ElliotXLeo <elliotgaramendi@gmail.com>
2022-05-26 00:41:28 -0500

    Update page PokemonsRead

--- src/pages/pokemons/PokemonsRead.jsx
+++ src/pages/pokemons/PokemonsRead.jsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import PokemonsCard from "../../component/pokemons/PokemonsCard";
 import usePokemon from "../../hooks/usePokemon";
 
@@ -10,7 +9,7 @@ const PokemonsRead = () => {
     <section className="py-8">
       <div className="flex flex-col gap-8 container mx-auto px-8 md:px-4">
         <h2 className="text-3xl text-yellow-400 font-bold">Pokémons ({pokemons.length})</h2>
-        <div className="grid justify-center md:grid-cols-4 gap-8">
+        <div className="grid justify-items-center md:grid-cols-3 gap-8">
           {
             pokemons.map((pokemon) => {
               return (
